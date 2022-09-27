867c4c425baf053c8f68a1b37cac74bd95e80492
MAlejnadroR <manuelromeromiguel@gmail.com>
2022-09-27 06:59:46 +0200

    recuperado del correo

---
+++ slide_config.js
@@ -0,0 +1,91 @@
+var SLIDE_CONFIG;
+SLIDE_CONFIG = {
+    // Slide settings
+    settings: {
+
+        title: 'DESARROLLO DE APLICACIONES WEB. CPIFP Los Enlaces',
+        subtitle: 'Presentación del ciclo, curso 2019-2020 <br /> Modalidad Distancia ',
+        other: 'LA educación a distancia: todo un reto ',
+//    eventInfo: {
+//      title: 'ITERNOVA',
+//      date: '2014-01-15'
+//    },
+        useBuilds: true, // Default: true. False will turn off slide animation builds.
+        usePrettify: true, // Default: true
+        enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+        enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+        //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
+        favIcon: 'images/favicon.ico',
+        fonts: [
+            'Open Sans:regular,semibold,italic,italicsemibold',
+            'Source Code Pro'
+        ],
+        //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+    },
+
+    // Author information
+    presenters: [
+
+        /*
+    {  name: 'Cristina Barranco',
+      company: 'Lenguajes de Marcas',
+      //gplus: 'http://plus.google.com/1234567890',
+      //email: 'email@email.com',
+  }, {
+      name: 'Sofía Agustín Soro',
+      company: 'Entornos de desarrollo',
+      //gplus: 'http://plus.google.com/1234567890',
+      //email: 'email@email.com',
+  }, {
+      name: 'Santiago López',
+      company: 'Sistemas de información',
+      //gplus: 'http://plus.google.com/1234567890',
+      //email: 'email@email.com',
+  }, {
+      name: 'Francisco Pérez',
+      company: 'Bases de datos- Programación',
+      //gplus: 'http://plus.google.com/1234567890',
+      //email: 'email@email.com',
+  }, {
+      name: 'Lola Macipe',
+      company: 'Formación y orientación laboral'
+      //gplus: 'http://plus.google.com/1234567890',
+      //email: 'email@email.com',
+  },{   name: 'Teresa Latre Pintado',
+    company: 'Inglés 1 - Inglés 2'
+//gplus: 'http://plus.google.com/1234567890',
+//email: 'email@email.com',
+},
+      {
+      name: 'Miguel Robles',
+      company: 'DAWCliente - Desarrollo de interfaces'
+      //gplus: 'http://plus.google.com/1234567890',
+      //email: 'email@email.com',
+  }, {
+      name: 'Ana Aparicio Clavería',
+      company: 'Empresa e iniciativa emprendedora'
+      //gplus: 'http://plus.google.com/1234567890',
+      //email: 'email@email.com',
+  }, {
+      name: 'Teresa Martínez',
+      company: 'Despliegue de aplicaciones web'
+      //gplus: 'http://plus.google.com/1234567890',
+      //email: 'email@email.com',
+  },*/{
+            name: 'Tutor:    Manuel Alejandro Romero Miguel',
+            company: 'ManuelRomeroMiguel@gmail.com',
+            //gplus: 'http://plus.google.com/1234567890',
+            email: 'ManuelRomeroMiguel   @   gmail . com',
+            twitter: '@ManuelRomero',
+            www: 'http://es.wikieducator.org/Usuario:ManuelRomero',
+            blog: 'http://es.wikieducator.org/Usuario:ManuelRomero'
+            //phone: '(+34) 679 464 301',
+            //github: 'http://github.com/you'
+        },
+
+
+    ]
+
+
+};
+
